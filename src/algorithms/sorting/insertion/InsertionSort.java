fa80507daf11b77356eb08810418bcaac031470e
Sandra Binoy <sandra.binoy@outlook.com>
2021-04-10 22:03:26 +0530

    Completed InsertionSort.java

--- src/algorithms/sorting/insertion/InsertionSort.java
+++ src/algorithms/sorting/insertion/InsertionSort.java
@@ -5,8 +5,21 @@ public class InsertionSort {
     public int[] sort(int[] array) {
 
         int n = array.length;
+        int temp = 0;
 
+        for (int i = 1; i < n; i++) {
 
+            temp = array[i];
+            int j = 0;
+
+            for (j = i; j > 0 && temp < array[j-1]; j--) {
+
+                array[j] = array[j-1];
+
+            }
+            array[j] = temp;
+
+        }
 
         return array;
 
