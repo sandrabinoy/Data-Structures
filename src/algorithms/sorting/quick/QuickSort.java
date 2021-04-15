0403b39b64d546bdb596b96c344d5f8746e11a5f
Sandra Binoy <sandra.binoy@outlook.com>
2021-04-15 19:09:10 +0530

    Quick Sort added

--- src/algorithms/sorting/quick/QuickSort.java
+++ src/algorithms/sorting/quick/QuickSort.java
@@ -1,4 +1,15 @@
 package algorithms.sorting.quick;
 
 public class QuickSort {
+
+    public int[] sort(int[] array, int start, int end) {
+
+        if((end-start) < 2) {
+            return null;
+        }
+
+
+
+    }
+
 }
