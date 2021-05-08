1558ffd7fe30d32713b0e290bddf1572eca4c8a8
Sandra Binoy <sandra.binoy@outlook.com>
2021-05-08 19:53:45 +0530

    MaxDiffPrime updated (Comments)

--- src/problems/maxdiff/MaxDiffPrime.java
+++ src/problems/maxdiff/MaxDiffPrime.java
@@ -17,29 +17,19 @@ public class MaxDiffPrime {
 
         System.out.println("Left Prime: "+leftPrime + "\tRight Prime: "+rightPrime);
         if(leftPrime == 0) {
-
             if(rightPrime == 0) {
-
                 return maxDiff;
-
             }
-
         }
 
         else {
-
             if(leftPrime == rightPrime) {
-
                 return maxDiff = 0;
-
             }
 
             else {
-
                 maxDiff = rightPrime - leftPrime;
-
             }
-
         }
 
         return maxDiff;
@@ -48,76 +38,53 @@ public class MaxDiffPrime {
 
     public int findNearestPrime (int left, int right, boolean fromLeft) {
 
-        int average = (right - left) / 2;
         int primeNumber = 0;
         int flag = -1;
 
         //Go from left to right
         if(fromLeft == true) {
-
             //If left is 2, which is the only even prime number
             if(left == 2) {
-
                 return primeNumber = left;
-
             }
 
             //Making sure that the left is an odd number, else incrementing
             if(left%2 == 0) {
-
                 ++left;
-
             }
 
             for(int leftIndex = left; leftIndex <= right; leftIndex+=2) {
-
                 flag = 1;
                 for(int j = 2; j*j <= leftIndex; ++j) {
-
                     if(leftIndex % j == 0) {
-
                         flag = 0;
                         break;
-
                     }
-
                 }
 
                 if(flag == 1) {
-
                     return primeNumber = leftIndex;
-
                 }
-
             }
-
         }
 
         //Go from right to left
         else {
-
             if(right%2 == 0) {
                 --right;
             }
 
             for(int rightIndex = right; rightIndex >= left; rightIndex-=2) {
-
                 flag = 1;
                 for(int j = 2; j*j <= rightIndex; ++j) {
-
                     if(rightIndex % j == 0) {
-
                         flag = 0;
                         break;
-
                     }
-
                 }
 
                 if(flag == 1) {
-
                     return primeNumber = rightIndex;
-
                 }
 
             }
