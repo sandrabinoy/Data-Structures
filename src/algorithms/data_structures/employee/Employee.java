84c9d74abec4e2cae5c047c8b1d5f6fbf161a796
Sandra Binoy <sandra.binoy@outlook.com>
2021-06-23 21:59:06 +0530

    Lists

---
+++ src/algorithms/data_structures/employee/Employee.java
@@ -0,0 +1,48 @@
+package algorithms.data_structures.employee;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private Integer id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee(String firstName, String lastName, Integer id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+}
---
+++ src/algorithms/data_structures/list/Lists.java
@@ -0,0 +1,33 @@
+package algorithms.data_structures.list;
+
+import algorithms.data_structures.employee.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Sandra", "Binoy", 001));
+        employeeList.add(new Employee("Wynter", "Binoy", 002));
+        employeeList.add(new Employee("Sommer", "Binoy", 003));
+        employeeList.add(new Employee("Astrid", "Binoy", 004));
+
+        System.out.println(employeeList.get(1));
+
+        System.out.println(employeeList.isEmpty());
+
+        employeeList.set(3, new Employee("Astrid", "S", 005));
+
+        System.out.println(employeeList.size());
+
+        employeeList.add(3, new Employee("Astrid", "Binoy", 4));
+        employeeList.remove(4);
+
+        employeeList.forEach(employee -> System.out.println(employee));
+
+    }
+
+}
